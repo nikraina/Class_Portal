04a590d81b514b037a39a4d0736bc43f218cff72
nraina <raina.nith@gmail.com>
2016-02-05 16:51:13 -0500

    Basic Skeleton of our app

---
+++ Skeleton_For_Message_Board/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MessengerApp::Application.config.secret_key_base = 'd0207c0a155fd76b410924f0053d02baa8b004e61a87c123599a5e1f3f2181cecaab2e2d95576ca4beabf1f37aeaeeb1bb180879fdd2134bbdc260b5fd805836'
---
+++ Skeleton_For_Message_Board/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MessengerApp::Application.config.session_store :cookie_store, key: '_MessengerApp_session'
