47612a99cce28cc6f38f2daf59c22a0496a1aa45
dasoni <dasoni@ncsu.edu>
2016-02-25 19:52:29 -0500

    Test Cases Added

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,9 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  setup do
-    @user = users(:one)
+  def setup
+    @user = User.create(name: "Example User", email: "user@example.com",
+                     password: "password")
   end
 
   test "should get index" do
@@ -18,10 +19,10 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should create user" do
     assert_difference('User.count') do
-      post :create, user: {  }
+      post :create, user: { email: @user.email, is_admin: @user.is_admin, name: @user.name, password: @user.password }
     end
 
-    assert_redirected_to user_path(assigns(:user))
+    assert_response :redirect
   end
 
   test "should show user" do
@@ -31,19 +32,18 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should get edit" do
     get :edit, id: @user
-    assert_response :success
+    assert_response :redirect
   end
 
   test "should update user" do
-    patch :update, id: @user, user: {  }
-    assert_redirected_to user_path(assigns(:user))
+    patch :update, id: @user, user: { email: @user.email, is_admin: @user.is_admin, name: @user.name, password: @user.password }
+    assert_response :redirect
   end
 
   test "should destroy user" do
     assert_difference('User.count', -1) do
       delete :destroy, id: @user
     end
-
-    assert_redirected_to users_path
+    assert_response :redirect
   end
 end
